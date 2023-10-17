7b1d340bf61e5b8951116591eb57b41abf319180
Sergio Aguero <sergioaguerovalle@gmail.com>
2023-10-17 14:27:49 -0600

    10-17-23(1)

--- proyectoprogra3/src/main/java/com/proyectoprogra3/proyectoprogra3/Notes.java
+++ proyectoprogra3/src/main/java/com/proyectoprogra3/proyectoprogra3/Notes.java
@@ -1,10 +1,11 @@
 package com.proyectoprogra3.proyectoprogra3;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
+import org.springframework.util.comparator.BooleanComparator;
+
 public class Notes {
-    // Variables
+    //************************************************************Variables************************************************************
     private String noteText;
     private String noteBackgroundColor;
     private String noteTitle;
@@ -13,7 +14,7 @@ public class Notes {
     private String noteCategory;
     private List<String> noteSharedWithListOfUsers;
 
-    //Contructors
+    //************************************************************Contructors************************************************************
     public Notes(){
         this.noteSharedWithListOfUsers = new ArrayList<>();
         this.noteText = "";
@@ -37,7 +38,64 @@ public class Notes {
         this.noteCategory = noteCategory;
     }
 
-    //Setters and Getters
+    //************************************************************Setters and Getters************************************************************
+    //##################################################################Setters##################################################################
+    public void setNoteText(String noteText){
+        this.noteText = noteText;
+    }
 
+    public void setNoteBackgroundColor(String noteBackgroundColor){
+        this.noteBackgroundColor = noteBackgroundColor;
+    }
     
+    public void setNoteTitle(String noteTitle){
+        this.noteTitle = noteTitle;
+    }
+
+    public void setNotePasswordEnabled(Boolean notePasswordEnabled){
+        this.notePasswordEnabled = notePasswordEnabled;
+    }
+
+    public void setNotePassword(String notePassword){
+        this.notePassword = notePassword;
+    }
+
+    public void setNoteCategory(String noteCategory){
+        this.noteCategory = noteCategory;
+    }
+
+    public void setNoteSharedWithListOfUsers(List<String> noteSharedWithListOfUsers){
+        this.noteSharedWithListOfUsers = noteSharedWithListOfUsers;
+    }
+    //##################################################################Getters##################################################################
+    public String getNoteText(){
+        return noteText;
+    }
+
+    public String getNoteBackgroundColor(){
+        return noteBackgroundColor;
+    }
+    
+    public String getNoteTitle(){
+        return noteTitle;
+    }
+
+    public Boolean getNotePasswordEnabled(){
+        return notePasswordEnabled;
+    }
+
+    public String getNotePassword(){
+        return notePassword;
+    }
+
+    public String getNoteCategory(){
+        return noteCategory;
+    }
+
+    public List<String> getNoteSharedWithListOfUsers(){
+        return noteSharedWithListOfUsers;
+    }
+
+    //************************************************************Class Funtions************************************************************
+
 }
